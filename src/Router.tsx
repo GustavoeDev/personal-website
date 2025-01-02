3bc9d4045e1d4f5a1c8ce5fb35afda0d651962cc
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-02 18:54:59 -0300

    feat: add new pages and routes for Projects, About, and Contact

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Home } from "./pages/Home";
 import { DefaultLayout } from "./layouts/DefaultLayout";
+
+import { Home } from "./pages/Home";
+import { Projects } from "./pages/Projects";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/projetos" element={<Projects />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/contato" element={<Contact />} />
       </Route>
     </Routes>
   );
