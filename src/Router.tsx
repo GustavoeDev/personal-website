33dffd47dc9ad9a9295b19a3884744f9ee26ca97
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-03 17:03:46 -0300

    refactor: remove DefaultLayout and simplify routing structure

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 
-import { DefaultLayout } from "./layouts/DefaultLayout";
-
 import { Home } from "./pages/Home";
 import { Projects } from "./pages/Projects";
 import { About } from "./pages/About";
@@ -10,12 +8,10 @@ import { Contact } from "./pages/Contact";
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/projetos" element={<Projects />} />
-        <Route path="/sobre" element={<About />} />
-        <Route path="/contato" element={<Contact />} />
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/projetos" element={<Projects />} />
+      <Route path="/sobre" element={<About />} />
+      <Route path="/contato" element={<Contact />} />
     </Routes>
   );
 }
