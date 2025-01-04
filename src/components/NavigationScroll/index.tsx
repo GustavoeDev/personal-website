6ee44c30da229fb1318891f7a65a0e02c3b2c3e9
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-03 23:41:34 -0300

    feat: implement navigation modal with scroll visibility and social links

--- src/components/NavigationScroll/index.tsx
+++ src/components/NavigationScroll/index.tsx
@@ -1,17 +1,31 @@
 import { useEffect, useState } from "react";
 
-import { List } from "phosphor-react";
+import { List, X } from "phosphor-react";
 import { HoverEffect } from "../HoverEffect";
-import { NavigationButton } from "./styles";
+
+import {
+  NavigationButton,
+  Navigation,
+  NavigationContent,
+  NavigationTitle,
+  NavigationSocials,
+  NavigationLinks,
+} from "./styles";
+
+import { useLocation } from "react-router-dom";
 
 export function NavigationScroll() {
   const [isVisible, setIsVisible] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const location = useLocation();
 
   const handleScroll = () => {
     if (window.scrollY > 100) {
       setIsVisible(true);
     } else {
       setIsVisible(false);
+      setIsModalOpen(false);
     }
   };
 
@@ -23,9 +37,83 @@ export function NavigationScroll() {
     };
   }, []);
 
+  function toggleModal() {
+    setIsModalOpen(!isModalOpen);
+  }
+
   return (
-    <HoverEffect as={NavigationButton} className={isVisible ? "visible" : ""}>
-      <List size={24} />
-    </HoverEffect>
+    <>
+      <HoverEffect
+        as={NavigationButton}
+        onClick={toggleModal}
+        className={
+          isVisible && isModalOpen
+            ? "visible active"
+            : isVisible
+            ? "visible"
+            : ""
+        }
+      >
+        {isModalOpen ? <X size={24} /> : <List size={24} />}
+      </HoverEffect>
+
+      <Navigation className={isModalOpen ? "visible" : ""}>
+        <NavigationContent>
+          <NavigationTitle>
+            <p>NAVEGAÇÃO</p>
+            <hr />
+          </NavigationTitle>
+          <NavigationLinks>
+            <HoverEffect
+              href="/"
+              className={location.pathname === "/" ? "active" : ""}
+            >
+              Início
+            </HoverEffect>
+            <HoverEffect
+              href="/projetos"
+              className={location.pathname === "/projetos" ? "active" : ""}
+            >
+              Projetos
+            </HoverEffect>
+            <HoverEffect
+              href="/sobre"
+              className={location.pathname === "/sobre" ? "active" : ""}
+            >
+              Sobre
+            </HoverEffect>
+            <HoverEffect
+              href="/contato"
+              className={location.pathname === "/contato" ? "active" : ""}
+            >
+              Contato
+            </HoverEffect>
+          </NavigationLinks>
+          <NavigationSocials>
+            <p>REDES SOCIAIS</p>
+            <div>
+              <HoverEffect
+                href="https://github.com/GustavoeDev/"
+                target="_blank"
+              >
+                Github
+              </HoverEffect>
+              <HoverEffect
+                href="https://www.linkedin.com/in/gustavo-emanuel-52bb29344/"
+                target="_blank"
+              >
+                Linkedin
+              </HoverEffect>
+              <HoverEffect
+                href="https://www.instagram.com/gustavox8_/"
+                target="_blank"
+              >
+                Instagram
+              </HoverEffect>
+            </div>
+          </NavigationSocials>
+        </NavigationContent>
+      </Navigation>
+    </>
   );
 }
--- src/components/NavigationScroll/styles.ts
+++ src/components/NavigationScroll/styles.ts
@@ -17,6 +17,10 @@ export const NavigationButton = styled.button`
   opacity: 0;
   visibility: hidden;
 
+  &.active {
+    background-color: ${(props) => props.theme["blue-500"]};
+  }
+
   &.visible {
     opacity: 1;
     visibility: visible;
@@ -28,5 +32,121 @@ export const NavigationButton = styled.button`
 
   &:hover {
     background-color: ${(props) => props.theme["blue-500"]};
+  }
+`;
+
+export const Navigation = styled.div`
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  right: 0;
+  max-width: 32rem;
+  width: 100%;
+  height: 100vh;
+  background-color: ${(props) => props.theme["dark-500"]};
+
+  transition: clip-path 1s ease-out;
+  clip-path: ellipse(0% 70% at 200% 50%);
+
+  &.visible {
+    clip-path: ellipse(150% 100% at 100% 50%);
+  }
+`;
+
+export const NavigationContent = styled.div`
+  padding: 6rem;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2.5rem;
+`;
+
+export const NavigationTitle = styled.div`
+  color: ${(props) => props.theme["gray-500"]};
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  p {
+    font-size: 0.65rem;
+    font-weight: 700;
+  }
+
+  hr {
+    border: none;
+    height: 1px;
+    background-color: ${(props) => props.theme["gray-500"]};
+  }
+`;
+
+export const NavigationLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
+
+  a {
+    font-size: 3rem;
+    position: relative;
+  }
+
+  a::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: -1.5rem;
+    width: 8px;
+    height: 8px;
+    background-color: ${(props) => props.theme["white"]};
+    border-radius: 50%;
+    transform: translateY(-50%) scale(0);
+    transition: transform 0.4s ease;
+  }
+
+  a.active::after,
+  a:hover::after {
+    transform: translateY(-50%) scale(1);
+  }
+`;
+
+export const NavigationSocials = styled.div`
+  color: ${(props) => props.theme["gray-500"]};
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  p {
+    font-size: 0.65rem;
+    font-weight: 700;
+  }
+
+  & > div {
+    display: flex;
+    gap: 1.5rem;
+
+    a {
+      font-size: 0.875rem;
+      position: relative;
+      padding-bottom: 0.5rem;
+    }
+
+    a::after {
+      content: " ";
+      width: 0%;
+      height: 1px;
+      background-color: ${(props) => props.theme["white"]};
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      transition: 0.3s ease-in-out;
+    }
+
+    a:hover::after {
+      width: 100%;
+    }
   }
 `;
